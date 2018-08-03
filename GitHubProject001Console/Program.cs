6e63c6c4d9bf66b7de74e4f0a2189df2038748f9
darias01 <daniel.arias@tss.com.pe>
2018-08-03 16:27:04 -0500

    branchA02 : commit 20180803 1627

--- GitHubProject001Console/Program.cs
+++ GitHubProject001Console/Program.cs
@@ -17,6 +17,8 @@ namespace GitHubProject001Console
             Console.WriteLine("testing ... branch03 : commit 20180802 1543");
             Console.WriteLine("testing ... branch03 : commit 20180802 1610");
             Console.WriteLine("testing ... branch04 : commit 20180802 1622");
+
+            Console.WriteLine("testing ... branchA02 : commit 20180803 1627");
         }
     }
 }
